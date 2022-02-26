01014fb3970a0f07223d9365abd0bb0a67b0e0c0
Redbeard86 <92319280+Redbeard86@users.noreply.github.com>
2022-02-26 09:59:52 -0600

    Update InnerJoin.sql

--- InnerJoin.sql
+++ InnerJoin.sql
@@ -1,3 +1,17 @@
+Select employees and managers with inner join
+
+The Employee table has the following columns:
+
+ID - integer, primary key
+FirstName - variable-length string
+LastName - variable-length string
+ManagerID - integer
+
+Write a SELECT statement to show a list of all employees' first names and their managers' first names. List only employees that have a manager. 
+Order the results by Employee first name. Use aliases to give the result columns distinctly different names, like "Employee" and "Manager".
+
+
+
 -- Your SELECT statement goes here
 Select E.FirstName AS Employee, M.FirstName AS Manager
 From Employee E
